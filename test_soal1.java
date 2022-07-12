3fdf6c30bd7028dc60b2a051bf56ce2a1147fc2c
lefrandbima_ <91729700+lefrandbima@users.noreply.github.com>
2022-07-12 19:01:25 +0700

    Update test_soal1.java

--- test_soal1.java
+++ test_soal1.java
@@ -1,14 +1,18 @@
-import java.util.stream.IntStream;
- 
-public class Soal1 
-{
-  public static void main(String[] args) 
-  {
-    fizzBuzz_(200);
-  }
-  private static void fizzBuzz_(int num) {
-    IntStream.rangeClosed(1, 200)
-        .mapToObj(i -> i % 5 == 0 ? (i % 11 == 0 ? "FizzBuzz" : "Fizz") : (i % 11 == 0 ? "Buzz" : i))
-        .forEach(System.out::println);
-  }
+public class Soal_1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 3, 10, 11, 20, 5, 100, 200};
+        for(int i = 0; i <= arr.length; i++)
+            if(i % 5 == 0 && i % 11 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 11 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(i);
+            }
+    }
 }
